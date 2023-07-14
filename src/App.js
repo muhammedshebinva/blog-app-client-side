d09e94477a13bc9798b1916fdac20bc01a7b07c2
muhammedshebinva <muhammedshebinva@gmail.com>
2023-07-14 10:02:18 +0530

    cleaned and started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <header>
+    <a href="" className="logo" >MyBlog</a>
+
+    <nav>
+      <a href="">login</a>
+      <a href="">Register</a>
+    </nav>
+    </header>
+    </main>
   );
 }
 
